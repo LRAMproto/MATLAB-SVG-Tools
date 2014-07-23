a4059bf9d3c189f9fc1b893272c89a90c264c40f
caster <caster@LRAM-dev-1.(none)>
2014-07-23 14:20:46 -0700

    Made a commit that should have been made earlier.

--- svg2jpg/svg2jpg.m
+++ svg2jpg/svg2jpg.m
@@ -1,9 +1,20 @@
-function svg2jpg(infile, outfile, resolution)
+function svg2jpg(varargin)
 % Renders SVG images at a given DPI.
-    previous = pwd;
-    cd(fileparts(mfilename('fullpath')))
-    javaaddpath(pwd);
-    cd(previous);
+
+
+if (length(varargin) == 2)
+    infile = varargin{1};
+    outfile = varargin{2};
+
+    % The following adds the Java class to the Java path. It briefly 
+    % changes directories to the install path, adds the path as necessary, 
+    % then changes back.
+    if (exist('SaveAsJPEG','class') ~= 8)
+        previous = pwd;
+        cd(fileparts(mfilename('fullpath')));
+        javaaddpath(pwd);
+        cd(previous);
+    end
     
     % Verifies structure of input file.
     
@@ -15,7 +26,7 @@ function svg2jpg(infile, outfile, resolution)
     if strcmp(inFileName,'')
         error('Please specify a filename');
     end
-
+    
     if strcmp(inExtension,'')
         inExtension = '.svg';
         
@@ -24,30 +35,40 @@ function svg2jpg(infile, outfile, resolution)
     end
     
     infile = fullfile(inPath, strcat(inFileName, inExtension));
-
-    % Verifies structure of output file.   
     
-    [outPath, outFileName, outExtension] = fileparts(outfile);    
-
-   if strcmp(outPath,'')
+    % Verifies structure of output file.
+    
+    [outPath, outFileName, outExtension] = fileparts(outfile);
+    
+    if strcmp(outPath,'')
         outPath = pwd;
     end
     
     if strcmp(outFileName,'')
         error('Please specify a filename');
     end
-
+    
     if strcmp(outExtension,'')
-        outExtension = '.svg';
+        outExtension = '.jpeg';
         
     elseif ~(strcmp(outExtension,'.jpeg') || strcmp(outExtension,'.jpg'))
         error('This may not be a JPEG file.');
     end
-
-    outfile = fullfile(outPath, strcat(outFileName, outExtension));    
-
+    
+    outfile = fullfile(outPath, strcat(outFileName, outExtension));
+    
     converter = SaveAsJPEG;
-    converter.main({infile, outfile});    
-
+    converter.main({infile, outfile});
+end
 end
 
+function val = isValidOpt(str)
+opts = {'InputFile','OutputFile','Resolution'};
+val = 0;
+for i=1:length(opts)
+    if strcmp(str,opts{i})
+        val = 1;
+        break;
+    end
+end
+end
